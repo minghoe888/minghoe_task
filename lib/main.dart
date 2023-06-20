22b3333190e589d1eecb841e76e47499cbab4384
minghoe888 <minghoe888@gmail.com>
2023-06-20 22:38:07 +0800

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'Page/FirstPage.dart';
+
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Comments Manager',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const FirstPage(title: 'Comments Manager'),
+    );
+  }
+}
+
+
